d1b6cb0a5ef46a2cac25401458228845fdc1a36a
Andi Rustianto <anditodev46@gmail.com>
2022-10-12 20:27:59 +0700

    [ADD] UI Onboarding One

---
+++ app/src/main/java/com/anditorx/movtestkt/onboarding/OneboardingOneActivity.kt
@@ -0,0 +1,12 @@
+package com.anditorx.movtestkt.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anditorx.movtestkt.R
+
+class OneboardingOneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_oneboarding_one)
+    }
+}
---
+++ app/src/main/java/com/anditorx/movtestkt/onboarding/OneboardingTwoActivity.kt
@@ -0,0 +1,12 @@
+package com.anditorx.movtestkt.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anditorx.movtestkt.R
+
+class OneboardingTwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_oneboarding_two)
+    }
+}
